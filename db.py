703216d17ae0e11e512da2506519fa06ba5175ba
alexethomas <alext@lshift.net>
2016-09-01 12:09:24 +0100

    Change db name

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ class DefaultTablenameMixin(object):
     def __tablename__(cls):
         return cls.__name__.lower()
 
-engine = create_engine('postgresql://postgres:password@localhost:5432/modular_analytics')
+engine = create_engine('postgresql://postgres:password@localhost:5432/sqlalchemy_analytics')
 session = sessionmaker(bind=engine)()
 Base = declarative_base(bind=engine, cls=DefaultTablenameMixin)
 
